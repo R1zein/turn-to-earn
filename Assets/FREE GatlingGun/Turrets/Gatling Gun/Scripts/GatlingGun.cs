20bc305755d1944f7437ac7b39defc0ab31d9611
Sawyerinvi <Eugene_tut@mail.ru>
2024-04-08 22:41:35 +0500

    Cannon

--- Assets/FREE GatlingGun/Turrets/Gatling Gun/Scripts/GatlingGun.cs
+++ Assets/FREE GatlingGun/Turrets/Gatling Gun/Scripts/GatlingGun.cs
@@ -1,87 +1,73 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEngine.GraphicsBuffer;
 
 public class GatlingGun : MonoBehaviour
 {
-    // target the gun will aim at
-    Transform go_target;
+    Transform target;
 
-    // Gameobjects need to control rotation and aiming
     public Transform go_baseRotation;
     public Transform go_GunBody;
     public Transform go_barrel;
 
-    // Gun barrel rotation
     public float barrelRotationSpeed;
     float currentRotationSpeed;
 
-    // Distance the turret can aim and fire from
     public float firingRange;
 
     // Particle system for the muzzel flash
     public ParticleSystem muzzelFlash;
 
-    // Used to start and stop the turret firing
     bool canFire = false;
+    private float timer;
 
     
     void Start()
     {
-        // Set the firing range distance
         this.GetComponent<SphereCollider>().radius = firingRange;
     }
 
     void Update()
     {
         AimAndFire();
+        timer += Time.deltaTime;
+        if (timer > 1f)
+        {
+            timer = 0f;
+            ScanForTarget();
+            if (target != null)
+            {
+                canFire = true;
+            }
+            else
+            {
+                canFire = false;
+            }
+        }
     }
 
     void OnDrawGizmosSelected()
     {
-        // Draw a red sphere at the transform's position to show the firing range
         Gizmos.color = Color.red;
         Gizmos.DrawWireSphere(transform.position, firingRange);
     }
 
-    // Detect an Enemy, aim and fire
-    void OnTriggerEnter(Collider other)
-    {
-        if (other.gameObject.tag == "Enemy")
-        {
-            go_target = other.transform;
-            canFire = true;
-        }
-
-    }
-    // Stop firing
-    void OnTriggerExit(Collider other)
-    {
-        if (other.gameObject.tag == "Enemy")
-        {
-            canFire = false;
-        }
-    }
 
     void AimAndFire()
     {
-        // Gun barrel rotation
         go_barrel.transform.Rotate(0, 0, currentRotationSpeed * Time.deltaTime);
 
-        // if can fire turret activates
         if (canFire)
         {
-            // start rotation
             currentRotationSpeed = barrelRotationSpeed;
 
-            // aim at enemy
-            Vector3 baseTargetPostition = new Vector3(go_target.position.x, this.transform.position.y, go_target.position.z);
-            Vector3 gunBodyTargetPostition = new Vector3(go_target.position.x, go_target.position.y, go_target.position.z);
+            Vector3 baseTargetPostition = new Vector3(target.position.x, this.transform.position.y, target.position.z);
+            Vector3 gunBodyTargetPostition = new Vector3(target.position.x, target.position.y, target.position.z);
 
             go_baseRotation.transform.LookAt(baseTargetPostition);
             go_GunBody.transform.LookAt(gunBodyTargetPostition);
 
-            // start particle system 
             if (!muzzelFlash.isPlaying)
             {
                 muzzelFlash.Play();
@@ -89,13 +75,28 @@ public class GatlingGun : MonoBehaviour
         }
         else
         {
-            // slow down barrel rotation and stop
             currentRotationSpeed = Mathf.Lerp(currentRotationSpeed, 0, 10 * Time.deltaTime);
 
-            // stop the particle system
             if (muzzelFlash.isPlaying)
             {
                 muzzelFlash.Stop();
+            }
+        }
+    }
+    private void ScanForTarget()
+    {
+        Collider[] colliders = Physics.OverlapSphere(transform.position, firingRange);
+        float shortestDistance = Mathf.Infinity;
+        foreach (Collider collider in colliders)
+        {
+            if (collider.TryGetComponent<EnemyNavigation>(out var bot))
+            {
+                float distance = Vector3.Distance(transform.position, bot.transform.position);
+                if (distance < shortestDistance)
+                {
+                    shortestDistance = distance;
+                    target = bot.transform;
+                }
             }
         }
     }
