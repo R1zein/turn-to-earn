f2b38efb6362223845a77b4ef387932660993e95
Sawyerinvi <Eugene_tut@mail.ru>
2024-05-06 22:09:54 +0500

    123

--- Assets/Scripts/Enemy/ZombieSpawn.cs
+++ Assets/Scripts/Enemy/ZombieSpawn.cs
@@ -9,21 +9,47 @@ public class ZombieSpawn : MonoBehaviour
     [SerializeField] private float maxSpawnTime;
     [SerializeField] private float spawnRate;
     [SerializeField] private Transform spawnPos;
+    private Animator animator;
     private float timer;
-
+    private float spawnTime;
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+        OpenPortal();
+    }
 
     private void Update()
     {
         timer += Time.deltaTime;
-        if(timer >= spawnRate)
-        {
-            timer = 0;
-            SpawnEnemy();
-        }
     }
 
     private void SpawnEnemy()
     {
         Instantiate(zombie, spawnPos.position, Quaternion.identity);
+        
+    }
+    public void OpenPortal()
+    {
+        animator.Play("OpenPortal");
+        spawnTime = Random.Range(minSpawnTime, maxSpawnTime);
+        timer = 0;
+        StartCoroutine(StartSpawning());
+    }
+    private void ClosePortal()
+    {
+        animator.Play("ClosePortal");
+        timer = 0;
+    }
+    private IEnumerator StartSpawning()
+    {
+        float length = animator.GetCurrentAnimatorStateInfo(0).length;
+        yield return new WaitForSeconds(length);
+        while (timer < spawnTime)
+        {
+            yield return new WaitForSeconds(spawnRate);
+            SpawnEnemy();
+        }
+        ClosePortal();
+        
     }
 }
