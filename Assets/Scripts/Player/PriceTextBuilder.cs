06c01586b7446346f44c41a71b5d5feddbb3e688
Sawyerinvi <Eugene_tut@mail.ru>
2024-09-17 21:43:08 +0500

    price builder

---
+++ Assets/Scripts/Player/PriceTextBuilder.cs
@@ -0,0 +1,14 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class PriceTextBuilder : MonoBehaviour
+{
+    public Ghost ghost;
+    public TMP_Text costText;
+    private void Start()
+    {
+        costText.text = $"Дерево: {ghost.requiredResources.tree} \n Железо: {ghost.requiredResources.iron} \n Камень: {ghost.requiredResources.ore}";
+    }
+
+}
+
