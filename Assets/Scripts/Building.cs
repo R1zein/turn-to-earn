48850189f1231b267abc875aa6b204c2ad29c660
R1zein <egorzaneytb@mail.ru>
2025-09-11 20:58:36 +0300

    11.09.zvukistukapoderevu

--- Assets/Scripts/Building.cs
+++ Assets/Scripts/Building.cs
@@ -2,5 +2,33 @@ using UnityEngine;
 
 public class Building : MonoBehaviour
 {
+    private StatsHandler statsHandler;
+    public AudioClip[] audioClips;
+    private AudioSource audioSource;
+    private void Awake()
+    {
+        statsHandler = GetComponent<StatsHandler>();
+        audioSource = GetComponent<AudioSource>();
+    }
+    private void OnEnable()
+    {
+        statsHandler.OnDeath += Death;
+        statsHandler.OnDamage += TakeDamage;
+    }
 
+    private void Death()
+    {
+        Destroy(gameObject);
+    }
+    private void OnDisable()
+    {
+        statsHandler.OnDeath -= Death;
+        statsHandler.OnDamage -= TakeDamage;
+    }
+
+    private void TakeDamage()
+    {
+        int randomIndex = Random.Range(0,audioClips.Length);
+        audioSource.PlayOneShot(audioClips[randomIndex]);
+    }
 }
