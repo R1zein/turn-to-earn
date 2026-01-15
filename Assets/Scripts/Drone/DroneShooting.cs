e26be07d65d6917a250b7fc48352ad6e5bf0e535
R1zein <egorzaneytb@mail.ru>
2026-01-15 20:52:20 +0300

    15.01

---
+++ Assets/Scripts/Drone/DroneShooting.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class DroneShooting : MonoBehaviour
+{
+    public Transform spr;
+    public Transform spl;
+    public float interval;
+    public float damage;
+
+    public void Shoot()
+    {
+        if (Input.GetMouseButton(0))
+        {
+
+        }
+    }
+}
